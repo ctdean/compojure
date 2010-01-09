698cf143e7e9ac7ba60f1800a975af5a8de1a1a9
Chris Dean <ctdean@kyluka.com>
2010-01-08 18:28:50 -0800

    Simple lein project for compojure

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject org.clojars.ctdean/compojure "0.3.1"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+                 [commons-codec "1.3"]
+                 [commons-io "1.4"]
+                 [commons-fileupload "1.2.1"]
+                 [org.mortbay.jetty/jetty "6.1.21"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
